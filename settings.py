03f87219688e74348740d9ae4ddcf0997f8c60c7
Chris Williams <17027446+mistwire@users.noreply.github.com>
2024-04-29 20:06:32 -0400

    fix number of bullets :-D

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 15
 
         # Alien settings
         self.fleet_drop_speed = 10
